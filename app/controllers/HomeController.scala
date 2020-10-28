b2f451a3a77e3261e39d86ec48bee58c6266aaec
Carolin König <carolin.koenig@htwg-konstanz.de>
2020-10-28 17:12:21 +0100

    Implement TUI as Text

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,14 +1,22 @@
 package controllers
 
 import javax.inject._
-
 import play.api.mvc._
+import de.htwg.se.rummi.Rummi
+import de.htwg.se.rummi.model.{Player}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val gameController = Rummi.controller
+  gameController.game.generateNewGame(Player("Patrick") :: Player("Carolin") :: Nil)
+  gameController.initGame("Patrick" :: "Carolin" :: Nil)
+  def rummiAsText = gameController.fieldToString + gameController.rackOfActivePlayerToString + gameController.gameStateToString
+
+  def rummi = Action {
+    Ok(rummiAsText)
+  }
 
   def about= Action {
     Ok(views.html.index())
   }
-
 }
