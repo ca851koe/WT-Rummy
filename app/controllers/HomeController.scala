2177affad205b6f55d3f06807b919ab483388cc9
Carolin König <carolin.koenig@htwg-konstanz.de>
2020-10-29 09:54:41 +0100

    more GETS for the TUI added

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,9 +10,42 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   val gameController = Rummi.controller
   gameController.game.generateNewGame(Player("Patrick") :: Player("Carolin") :: Nil)
   gameController.initGame("Patrick" :: "Carolin" :: Nil)
-  def rummiAsText = gameController.fieldToString + gameController.rackOfActivePlayerToString + gameController.gameStateToString
+
+  def rummiAsText = gameController.fieldToString + gameController.rackOfActivePlayerToString +
+    gameController.activePlayerToString + gameController.gameStateToString
+
 
   def rummi = Action {
+    Ok(rummiAsText)
+  }
+
+  def rummiSorted: Action[AnyContent] = Action {
+    gameController.sortRack
+    Ok(rummiAsText)
+  }
+
+  def newTile: Action[AnyContent] = Action {
+    gameController.draw
+    Ok(rummiAsText)
+  }
+
+  def switchPlayer: Action[AnyContent] = Action {
+    gameController.switchPlayer
+    Ok(rummiAsText)
+  }
+
+  def moveTile(from:String, to: String): Action[AnyContent] = Action {
+    gameController.moveTile(from, to)
+    Ok(rummiAsText)
+  }
+
+  def undoStep: Action[AnyContent] = Action {
+    gameController.undo
+    Ok(rummiAsText)
+  }
+
+  def redoStep: Action[AnyContent] = Action {
+    gameController.redo
     Ok(rummiAsText)
   }
 
